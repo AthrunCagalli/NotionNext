19e768c34bee78eca91a916f70fd76dcf4c49cfe
Mowgli <74955558+AthrunCagalli@users.noreply.github.com>
2023-10-19 15:40:54 +0800

    Update config.js 修改主题配置

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
   HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂动； false则随鼠标滚动 ;
   // 是否显示开始阅读按钮
-  SHOW_START_READING: true,
+  SHOW_START_READING: false,
 
   // 菜单配置
   MENU_INDEX: true, // 显示首页
@@ -28,10 +28,10 @@ const CONFIG = {
   ARTICLE_COPYRIGHT: true, // 显示文章版权声明
   ARTICLE_RECOMMEND: true, // 文章关联推荐
 
-  WIDGET_LATEST_POSTS: true, // 显示最新文章卡
+  WIDGET_LATEST_POSTS: false, // 显示最新文章卡
   WIDGET_ANALYTICS: false, // 显示统计卡
   WIDGET_TO_TOP: true,
-  WIDGET_TO_COMMENT: true, // 跳到评论区
+  WIDGET_TO_COMMENT: false, // 跳到评论区
   WIDGET_DARK_MODE: true, // 夜间模式
   WIDGET_TOC: true // 移动端悬浮目录
 }
